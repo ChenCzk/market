47154c6154da0d1e216087a0b282bb3068164bc4
zekaichen <915198279@qq.com>
2020-09-24 09:23:46 +0800

    测试

--- src/main/java/com/example/market/dao/uploadDao.java
+++ src/main/java/com/example/market/dao/uploadDao.java
@@ -14,10 +14,12 @@ public interface uploadDao {
     // 添加图片或图标
     @Insert("insert into photo_table(id,photo) values (#{id},#{photo})")
     void insertPhoto(@Param("id")String  id, @Param("photo") byte[] photo);
+
     // 添加文档
     @Insert("insert into word_table(id,word) values (#{id},#{word})")
     void insertWord(@Param("id")String  id, @Param("word") byte[] word);
 
+    // 改
     // 根据ID查看对应图片或图标
     @Select("Select photo from photo_table where id = #{id}")
     Object getPhotoByID(@Param("id") String id);
